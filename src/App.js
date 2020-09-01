fa9b2d18926200e41b14d559d5eccd04b0000090
Millie <millie_clare@outlook.com>
2020-09-01 15:41:18 +0100

    Removed 'contact'

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import About from './components/about/about';
 import Resume from './components/resume/resume';
 import Portfolio from './components/portfolio/portfolio';
 import Testimonials from './components/testimonials/testimonials';
-import ContactUs from './components/contact/contact';
 import Footer from './components/footer/footer';
 import './App.css';
 
@@ -17,7 +16,6 @@ function App() {
         <Resume />
         <Portfolio />
         <Testimonials />
-        <ContactUs />
         <Footer />
       </p>
     </div>
