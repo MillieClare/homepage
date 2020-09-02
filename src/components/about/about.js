d186d9340a168d5f71edc6e7467ab3af466e2b42
Millie <millie_clare@outlook.com>
2020-09-02 10:58:49 +0100

    spelling changes

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -9,7 +9,7 @@ export default function About() {
             <div className="nine columns main-col">
               <h2>About Me</h2>
               <p>I started tinkering with webdesign over ten years ago, but it never crossed my mind that it could be my career until I rediscovered coding in 2017. 
-                Now, I'm a London-based developer seeking meaningful challenges and opportunities, looking to help others a solve problems in new and exicting ways. 
+                Now, I'm a London-based developer seeking meaningful challenges and opportunities, looking to help others a solve problems in new and exciting ways. 
             </p>
               <div className="row">
                 <div className="columns contact-details">
