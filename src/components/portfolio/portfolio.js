5a24cc10d738ba244c70d54f93eb7f4056da59a3
Millie Clare <Millie_Clare@outlook.com>
2020-09-04 15:27:32 +0100

    Added FTF & to-do list

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,96 +1,99 @@
-import React from 'react';
+import React from "react";
 
 const MiniMiniBot = {
   itemHref: "#modal-01",
   imgSrc: "images/portfolio/hi.png",
   projectName: "Miniminibot",
   languagesUsed: "JavaScript"
-}
+};
 
 const ProjectTwo = {
   itemHref: "#modal-02",
   imgSrc: "images/portfolio/drum1.jpg",
   projectName: "JS Drumkit",
   languagesUsed: "HTML, JavaScript"
-}
+};
 
 const ProjectThree = {
   itemHref: "#modal-03",
-  imgSrc: "images/portfolio/judah.jpg",
-  projectName: "Judah",
-  languagesUsed: "Web Designs"
-}
+  imgSrc: "images/portfolio/todoimage.png",
+  projectName: "To-Do List",
+  languagesUsed: "React, JavaScript, CSS"
+};
 
 const ProjectFour = {
   itemHref: "#modal-04",
-  imgSrc: "images/portfolio/into-the-light.jpg",
+  imgSrc: "images/portfolio/FTF.png",
   projectName: "Into The Light",
   languagesUsed: "Photography"
-}
+};
 
 const ProjectFive = {
   itemHref: "#modal-05",
   imgSrc: "images/portfolio/farmerboy.jpg",
   projectName: "Farmer Boy",
   languagesUsed: "Branding"
-}
+};
 
 const ProjectSix = {
   itemHref: "#modal-06",
   imgSrc: "images/portfolio/girl.jpg",
   projectName: "Girl",
   languagesUsed: "Photography"
-}
+};
 
 const ProjectSeven = {
   itemHref: "#modal-07",
   imgSrc: "images/portfolio/origami.jpg",
   projectName: "Origami",
   languagesUsed: "Illustrations"
-}
+};
 
 const ProjectEight = {
   itemHref: "#modal-08",
   imgSrc: "images/portfolio/retrocam.jpg",
   projectName: "Retrocam",
   languagesUsed: "Web Development"
-}
+};
 
 const MiniMiniBotPopout = {
   projectId: "modal-01",
-  projectImage: "images/portfolio/hi.png", // can use different images if needed?
+  projectImage: "images/portfolio/hi.png",
   projectTitle: "MiniMiniBot",
-  projectDescription: "A chat bot written using Twitch's API to connect to chat which interacts with viewers and responds to commands.",
+  projectDescription:
+    "A chat bot written using Twitch's API to connect to chat which interacts with viewers and responds to commands. August 2018- Present",
   projectCategories: "JavaScript",
   githubLink: "https://github.com/MillieClare/miniminibot_"
-}
+};
 
 const ProjectTwoPopout = {
   projectId: "modal-02",
-  projectImage: "images/portfolio/drum2.jpg", // can use different images if needed?
+  projectImage: "images/portfolio/drum2.jpg",
   projectTitle: "JS Drumkit",
-  projectDescription: "Links the different parts of a drumkit to keyboard inputs.",
+  projectDescription:
+    "Links the different parts of a drumkit to keyboard inputs. October 2019",
   projectCategories: "HTML, JavaScript",
   githubLink: "https://github.com/MillieClare/JSDrumkit"
-}
+};
 
 const ProjectThreePopout = {
   projectId: "modal-03",
-  projectImage: "images/portfolio/modals/m-judah.jpg", // can use different images if needed?
-  projectTitle: "Project 3",
-  projectDescription: "Blablabla.",
-  projectCategories: "JavaScript",
-  githubLink: "github repo link"
-}
+  projectImage: "images/portfolio/modals/example.gif",
+  projectTitle: "To-do List",
+  projectDescription: "Creat tasks, mark them off and delete them. August 2020",
+  projectCategories: "React, JavaScript, CSS, Material.ui",
+  githubLink: "https://github.com/MillieClare/todolist"
+};
 
 const ProjectFourPopout = {
   projectId: "modal-04",
-  projectImage: "images/portfolio/modals/m-intothelight.jpg", // can use different images if needed?
-  projectTitle: "Project 4",
-  projectDescription: "Blablabla.",
-  projectCategories: "JavaScript",
-  githubLink: "github repo link"
-}
+  projectImage: "images/portfolio/modals/pizza.gif", // can use different images if needed?
+  projectTitle: "For the Floofs",
+  projectDescription:
+    "Get random dogs from an assortment. On mobile, they can be shared through social media. April 2019",
+  projectCategories: "JavaScript, HTML, CSS",
+  githubLink: "https://github.com/MillieClare/forthefloofs"
+};
 
 const ProjectFivePopout = {
   projectId: "modal-05",
@@ -99,7 +102,7 @@ const ProjectFivePopout = {
   projectDescription: "Blablabla.",
   projectCategories: "JavaScript",
   githubLink: "github repo link"
-}
+};
 
 const ProjectSixPopout = {
   projectId: "modal-06",
@@ -108,7 +111,7 @@ const ProjectSixPopout = {
   projectDescription: "Blablabla.",
   projectCategories: "JavaScript",
   githubLink: "github repo link"
-}
+};
 
 const ProjectSevenPopout = {
   projectId: "modal-07",
@@ -117,7 +120,7 @@ const ProjectSevenPopout = {
   projectDescription: "Blablabla.",
   projectCategories: "JavaScript",
   githubLink: "github repo link"
-}
+};
 
 const ProjectEightPopout = {
   projectId: "modal-08",
@@ -126,45 +129,48 @@ const ProjectEightPopout = {
   projectDescription: "Blablabla.",
   projectCategories: "JavaScript",
   githubLink: "github repo link"
-}
-
+};
 
 function PortfolioItem(props) {
-  return(
+  return (
     <div className="columns portfolio-item">
-    <div className="item-wrap">
-      <a href={props.itemHref} title>
-        <img alt="" src={props.imgSrc} />
-        <div className="overlay">
-          <div className="portfolio-item-meta">
-            <h5>{props.projectName}</h5>
-            <p>{props.languagesUsed}</p>
+      <div className="item-wrap">
+        <a href={props.itemHref} title>
+          <img alt="" src={props.imgSrc} />
+          <div className="overlay">
+            <div className="portfolio-item-meta">
+              <h5>{props.projectName}</h5>
+              <p>{props.languagesUsed}</p>
+            </div>
           </div>
-        </div>
-        <div className="link-icon"><i className="icon-plus" /></div>
-      </a>
+          <div className="link-icon">
+            <i className="icon-plus" />
+          </div>
+        </a>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
 function PortfolioPopup(props) {
   return (
     <div id={props.projectId} className="popup-modal mfp-hide">
-    <img className="scale-with-grid" src={props.projectImage} alt="" />
-    <div className="description-box">
-      <h4>{props.projectTitle}</h4>
-      <p>{props.projectDescription}</p>
-      <span className="categories"><i className="fa fa-tag" />{props.projectCategories}</span>
+      <img className="scale-with-grid" src={props.projectImage} alt="" />
+      <div className="description-box">
+        <h4>{props.projectTitle}</h4>
+        <p>{props.projectDescription}</p>
+        <span className="categories">
+          <i className="fa fa-tag" />
+          {props.projectCategories}
+        </span>
+      </div>
+      <div className="link-box">
+        <a href={props.githubLink}>Github</a>
+        <a className="popup-modal-dismiss">Close</a>
+      </div>
     </div>
-    <div className="link-box">
-      <a href={props.githubLink}>Github</a>
-      <a className="popup-modal-dismiss">Close</a>
-    </div>
-  </div>
-  )
+  );
 }
-
 
 // add props
 export default function Portfolio() {
@@ -174,15 +180,18 @@ export default function Portfolio() {
         <div className="twelve columns collapsed">
           <h1>Check Out Some of My Works.</h1>
           {/* portfolio-wrapper */}
-          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-          <PortfolioItem {...MiniMiniBot} />
-          <PortfolioItem {...ProjectTwo} />
-          <PortfolioItem {...ProjectThree} />
-          <PortfolioItem {...ProjectFour} />
-          <PortfolioItem {...ProjectFive} />
-          <PortfolioItem {...ProjectSix} />
-          <PortfolioItem {...ProjectSeven} />
-          <PortfolioItem {...ProjectEight} />
+          <div
+            id="portfolio-wrapper"
+            className="bgrid-quarters s-bgrid-thirds cf"
+          >
+            <PortfolioItem {...MiniMiniBot} />
+            <PortfolioItem {...ProjectTwo} />
+            <PortfolioItem {...ProjectThree} />
+            <PortfolioItem {...ProjectFour} />
+            <PortfolioItem {...ProjectFive} />
+            <PortfolioItem {...ProjectSix} />
+            <PortfolioItem {...ProjectSeven} />
+            <PortfolioItem {...ProjectEight} />
           </div>
         </div>
         <PortfolioPopup {...MiniMiniBotPopout} />
@@ -194,7 +203,6 @@ export default function Portfolio() {
         <PortfolioPopup {...ProjectSevenPopout} />
         <PortfolioPopup {...ProjectEightPopout} />
       </div>
-  </section> 
+    </section>
   );
-};
-
+}
