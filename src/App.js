c1d964f8f93f4e7ce11e0e4120d4cb30d5a740ab
Millie Clare <Millie_Clare@outlook.com>
2020-09-04 16:37:08 +0100

    Removed React logo & css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import Header from './components/header/header';
-import About from './components/about/about';
-import Resume from './components/resume/resume';
-import Portfolio from './components/portfolio/portfolio';
-import Testimonials from './components/testimonials/testimonials';
-import Footer from './components/footer/footer';
-import './App.css';
+import React from "react";
+import Header from "./components/header/header";
+import About from "./components/about/about";
+import Resume from "./components/resume/resume";
+import Portfolio from "./components/portfolio/portfolio";
+import Testimonials from "./components/testimonials/testimonials";
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
