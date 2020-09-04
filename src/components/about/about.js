bb4cb956f6f82de566388df4ac513628c8831c44
Millie Clare <Millie_Clare@outlook.com>
2020-09-04 15:27:45 +0100

    Updated CV link

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,32 +1,43 @@
-import React from 'react';
+import React from "react";
 export default function About() {
-    return (
-        <section id="about">
+  return (
+    <section id="about">
+      <div className="row">
+        <div className="three columns">
+          <img className="profile-pic" src="images/profile.jpg" alt="" />
+        </div>
+        <div className="nine columns main-col">
+          <h2>About Me</h2>
+          <p>
+            I started tinkering with webdesign over ten years ago, but it never
+            crossed my mind that it could be my career until I rediscovered
+            coding in 2017. Now, I'm a London-based developer seeking meaningful
+            challenges and opportunities, looking to help others a solve
+            problems in new and exciting ways.
+          </p>
           <div className="row">
-            <div className="three columns">
-              <img className="profile-pic" src="images/profile.jpg" alt="" />
+            <div className="columns contact-details">
+              <h2>Contact Details</h2>
+              <p className="address">
+                <span>Millie Clare</span>
+                <br />
+                <span>millie_clare@outlook.com</span>
+              </p>
             </div>
-            <div className="nine columns main-col">
-              <h2>About Me</h2>
-              <p>I started tinkering with webdesign over ten years ago, but it never crossed my mind that it could be my career until I rediscovered coding in 2017. 
-                Now, I'm a London-based developer seeking meaningful challenges and opportunities, looking to help others a solve problems in new and exciting ways. 
-            </p>
-              <div className="row">
-                <div className="columns contact-details">
-                  <h2>Contact Details</h2>
-                  <p className="address">
-                    <span>Millie Clare</span><br />
-                    <span>millie_clare@outlook.com</span>
-                  </p>
-                </div>
-                <div className="columns download">
-                  <p>
-                    <a href="https://github.com/MillieClare/CV/blob/master/CV_Aug_2020.pdf" className="button"><i className="fa fa-download" />Download Resume</a>
-                  </p>
-                </div>
-              </div>
+            <div className="columns download">
+              <p>
+                <a
+                  href="https://github.com/MillieClare/CV/blob/master/CV_2020.pdf"
+                  className="button"
+                >
+                  <i className="fa fa-download" />
+                  Download Resume
+                </a>
+              </p>
             </div>
           </div>
-        </section>
-    );
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/components/resume/cv.js
+++ src/components/resume/cv.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React from "react";
 
 export default function Cv() {
   return (
     <div className="row cv">
-      <div className="three columns header-col">
-      </div>
+      <div className="three columns header-col"></div>
       <div className="nine columns main-col">
         <div className="row item">
           <div className="twelve columns">
             <h3>CV</h3>
-            <p className="info">Last updated<span>•</span> <em className="date">August 2020</em></p>
+            <p className="info">
+              Last updated<span>•</span> <em className="date">August 2020</em>
+            </p>
             <p>
-              You can find my entire CV <a href="https://github.com/MillieClare/CV/blob/master/CV_Aug_2020.pdf">here</a>. <br />Alternatively, read below for a summary of my employment and skills.
-      </p>
+              You can find my entire CV{" "}
+              <a href="https://github.com/MillieClare/CV/blob/master/CV_2020.pdf">
+                here
+              </a>
+              . <br />
+              Alternatively, read below for a summary of my employment and
+              skills.
+            </p>
           </div>
         </div>
       </div>
